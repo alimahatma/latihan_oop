d14ffff3ceac3a9097241e1b99d0ab6e05781f18
alimahatma <laluallistilo@gmail.com>
2020-09-30 13:35:34 +0700

    mengenal kelas pada python

---
+++ Main0_class.py
@@ -0,0 +1,19 @@
+class Hero:
+    pass
+
+hero1 = Hero()
+hero2 = Hero()
+hero3 = Hero()
+
+hero1.name = "sniper"
+hero1.health = 100
+
+hero2.name = "seven"
+hero2.health = 200
+
+hero3.name = "ucup"
+hero3.health = 1000
+
+print(hero3)
+print(hero3.__dict__)
+print("nama Hiro 1 adalah : ", hero1.name)
